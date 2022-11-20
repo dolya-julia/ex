71034d8f588e16df90b4437a001b05f6942eeed9
dolya-julia <113310097+dolya-julia@users.noreply.github.com>
2022-11-20 11:21:44 +0300

    Add files via upload

---
+++ myscript.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    //Скрыть PopUp при загрузке страницы    
+    PopUpHide();
+});
+//Функция отображения PopUp
+function PopUpShow(){
+    $("#popup1").show();
+    history.pushState({is_popup_opened: true}, "First work | Отправка на почту", "/index.html/popup");
+}
+//Функция скрытия PopUp
+function PopUpHide(){
+    $("#popup1").hide();
+    history.back(); // 
+}
+// Обработка события нажатия кнопки назад в браузере
+window.onpopstate = (event) => {
+    if (event.state != null)
+    if (event.state["is_popup_opened"]) {
+        PopUpHide();
+    }
+};
